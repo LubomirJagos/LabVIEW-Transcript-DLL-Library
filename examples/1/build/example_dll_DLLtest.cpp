8465e1002567852317541e1e631ff7fc80b9a43e
LubomirJagos <lubomir.jagos@gmail.com>
2018-09-23 23:36:37 +0200

    Added documentation in slovak language.

--- examples/1/build/example_dll_DLLtest.cpp
+++ examples/1/build/example_dll_DLLtest.cpp
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <cstring>
-#include <string>
 #include "example_dll.h"
-using namespace std;
-int main(int argc, char* argv[])
+
+int main(void)
 {
         //LuboJ. don't forget LabVIEW transcriptor prefix for SubVIs!
         
-        /*
         LVSubVI_CppFunc();                            //there is used in call generated function prefix LVSubVI_
         for(int i = 0; i < 200; i++){
                 //there is used in call generated function prefix LVSubVI_
@@ -15,16 +12,4 @@ int main(int argc, char* argv[])
         }
 
         return 0;
-       */
-
-//  char str [80];
-string str = "";// str.resize(256);
-string str2 = "lubo";
-  printf ("Enter your family name: ");
-  str.resize(strlen(&str[0]) == 0?256:strlen(&str[0]));  scanf ("%s",&str[0]); str.resize(strlen(&str[0]));  
-  printf("Your name: %s, length is: %d", &str[0], str.length());
-  str2 = str + str2;
-  printf("2nd string: %s, length: %d\n",&str2[0],str2.length());
-  return 0;
 }
-
